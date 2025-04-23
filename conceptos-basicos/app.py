66aa49fc8c339a7df893278237f66027399c9689
matias <matiasjavierlucero@gmail.com>
2025-04-22 22:38:28 -0300

    requests

--- conceptos-basicos/app.py
+++ conceptos-basicos/app.py
@@ -1,31 +1,31 @@
-from flask import Flask, render_template
-import json
-app = Flask(__name__)
+from datetime import date
+import requests
 
+from flask import (
+    Flask, 
+    render_template,
+    request,
+)
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def acerca():
+    return render_template('acerca_de.html')
+
+@app.route('/users/')
+def usuarios():
+    cant = request.args.get('cant', 3, int)
+    response = requests.get(
+        f'https://randomuser.me/api/?results={cant}'
+    ).json()
+    list_usuarios = response['results']
     return render_template(
-        'index.html',
+        'users.html',
+        usuarios=list_usuarios,
+        hoy=date.today()
     )
-
-@app.route('/states')
-def states():
-    with open('data/states.json') as f:
-        data = json.load(f)
-
-        return render_template(
-            'states.html',
-            data=data['provincias'],
-        )
-
-
-@app.route('/welcome/<name>')
-def welcome(name):
-    return f"Welcome , {name}!"
-
-@app.route('/sum/<a>/<b>')
-def suma(a, b):
-    c = int(a) + int(b)
-    return str(c)
-
